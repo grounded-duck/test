c8f2bcf0098f5eb54cbda35b4ab047fce582776e
grounded-duck <victor.mat.eng@gmail.com>
2025-01-28 11:05:01 -0300

    Add files via upload

---
+++ api.php
@@ -0,0 +1,16 @@
+<?php
+// Exibindo uma string simples
+echo "Olá, mundo!";
+
+// Exibindo valores de variáveis
+$nome = "João";
+$idade = 25;
+echo "Meu nome é $nome e eu tenho $idade anos.";
+
+// Concatenando strings com o ponto (.)
+echo "Esta é " . "uma mensagem " . "concatenada.";
+
+// Utilizando tags HTML
+echo "<h1>Bem-vindo ao PHP</h1>";
+echo "<p>PHP é uma linguagem de script popular para desenvolvimento web.</p>";
+?>
